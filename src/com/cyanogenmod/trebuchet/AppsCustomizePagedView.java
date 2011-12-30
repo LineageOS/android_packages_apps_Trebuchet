f361be5d8f4018a9e5197b667729c1d63938b278
nebkat <nebkat@teamhacksung.org>
2011-12-30 18:58:41 +0000

    AppsCustomize: Apps Sort Mode Cling
    Change-Id: I07a08afb1f249f226693e935ad7ebc017f9c79b0

--- src/com/cyanogenmod/trebuchet/AppsCustomizePagedView.java
+++ src/com/cyanogenmod/trebuchet/AppsCustomizePagedView.java
@@ -192,6 +192,7 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
 
     // Cling
     private boolean mHasShownAllAppsCling;
+    private boolean mHasShownAllAppsSortCling;
     private int mClingFocusedX;
     private int mClingFocusedY;
 
@@ -481,6 +482,10 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
                     offset[0];
             pos[1] += offset[1];
             mLauncher.showFirstRunAllAppsCling(pos);
+        } else if (!mHasShownAllAppsSortCling && isDataReady() && testDataReady() &&
+                ((Cling) getTabHost().findViewById(R.id.all_apps_cling)).isDismissed()) {
+            mHasShownAllAppsSortCling = true;
+            mLauncher.showFirstRunAllAppsSortCling();
         }
     }
 
