a9a61def91a9c1e133b9ec7a53fb4856aec4fdbd
Danesh Mondegarian <daneshm90@gmail.com>
2013-09-11 22:36:12 -0700

    Trebuchet : Show notification for new icon packs
    Notify user of compatible icon packs and allow
    them to apply upon install.
    
    Change-Id: If8ac1f9b9be7ce9f215fb2ccc63e7fe88a185eba

---
+++ src/com/cyanogenmod/trebuchet/ApplyIconPackReceiver.java
@@ -0,0 +1,27 @@
+package com.cyanogenmod.trebuchet;
+
+import com.cyanogenmod.trebuchet.preference.PreferencesProvider;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+
+public class ApplyIconPackReceiver extends BroadcastReceiver {
+
+    public static final String EXTRA_ICON_PACK_NAME =
+            "icon_pack_name";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String pkgName = intent.getStringExtra(EXTRA_ICON_PACK_NAME);
+        if (!TextUtils.isEmpty(pkgName)) {
+            PreferencesProvider.Interface.General.setIconPack(context, pkgName);
+            NotificationManager notificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.cancelAll();
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    }
+}
