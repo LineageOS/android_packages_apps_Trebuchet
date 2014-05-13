9611b80f23d40b35c2704855974d51fefe3ea35b
Clark Scheff <clark@cyngn.com>
2014-05-13 12:50:59 -0700

    Use wallpaper picker added to WallpaperCropper apk.
    The code is still here for easier upstream merges.
    
    Change-Id: I97ec1492e8acf7ff6a5ceef00a68f47e6444bf50

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -249,6 +249,11 @@ public class Launcher extends Activity
     private static final String TOOLBAR_VOICE_SEARCH_ICON_METADATA_NAME =
             "com.android.launcher.toolbar_voice_search_icon";
 
+    private static final String WALLPAPER_PICKER_PACKAGE =
+            "com.android.wallpapercropper";
+    private static final String WALLPAPER_PICKER_ACTIVITY =
+            "com.android.wallpapercropper.WallpaperPickerActivity";
+
     public static final String SHOW_WEIGHT_WATCHER = "debug.show_mem";
     public static final boolean SHOW_WEIGHT_WATCHER_DEFAULT = false;
 
@@ -2751,7 +2756,7 @@ public class Launcher extends Activity
     }
 
     protected ComponentName getWallpaperPickerComponent() {
-        return new ComponentName(getPackageName(), LauncherWallpaperPickerActivity.class.getName());
+        return new ComponentName(WALLPAPER_PICKER_PACKAGE, WALLPAPER_PICKER_ACTIVITY);
     }
 
     /**
