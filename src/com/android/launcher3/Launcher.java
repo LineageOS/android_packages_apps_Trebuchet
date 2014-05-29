87d476c72f8a18c69d0d81e261c4adc35e19ff14
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2014-05-29 07:43:16 -0500

    Add 'OK Google' hotword detection service
    Requires Google Search to be installed
    
    Change-Id: Iab388ab601b2e493732774fd4e1603978e30e18b

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -106,6 +106,8 @@ import com.android.launcher3.PagedView.TransitionEffect;
 import com.android.launcher3.settings.SettingsActivity;
 import com.android.launcher3.settings.SettingsProvider;
 
+import com.google.android.hotword.client.HotwordServiceClient;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -349,6 +351,8 @@ public class Launcher extends Activity
     private HideFromAccessibilityHelper mHideFromAccessibilityHelper
         = new HideFromAccessibilityHelper();
 
+    private HotwordServiceClient mHotwordServiceClient;
+
     // Preferences
     private boolean mHideIconLabels;
 
@@ -451,6 +455,8 @@ public class Launcher extends Activity
 
         mSavedState = savedInstanceState;
         restoreState(mSavedState);
+
+        mHotwordServiceClient = new HotwordServiceClient(this);
 
         // Update customization drawer _after_ restoring the states
         if (mAppsCustomizeContent != null) {
@@ -965,6 +971,8 @@ public class Launcher extends Activity
         mWorkspace.onResume();
         mAppsCustomizeContent.onResume();
 
+        mHotwordServiceClient.requestHotwordDetection(true);
+
         //Close out TransitionEffects Fragment
         Fragment f = getFragmentManager().findFragmentByTag(
                 TransitionEffectsFragment.TRANSITION_EFFECTS_FRAGMENT);
@@ -982,6 +990,8 @@ public class Launcher extends Activity
         mPaused = true;
         mDragController.cancelDrag();
         mDragController.resetLastGestureUpTime();
+
+        mHotwordServiceClient.requestHotwordDetection(false);
 
         // We call onHide() aggressively. The custom content callbacks should be able to
         // debounce excess onHide calls.
@@ -1665,6 +1675,9 @@ public class Launcher extends Activity
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
 
+        mHotwordServiceClient.onAttachedToWindow();
+        mHotwordServiceClient.requestHotwordDetection(true);
+
         // Listen for broadcasts related to user-presence
         final IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_OFF);
@@ -1679,6 +1692,9 @@ public class Launcher extends Activity
     public void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         mVisible = false;
+
+        mHotwordServiceClient.onDetachedFromWindow();
+        mHotwordServiceClient.requestHotwordDetection(false);
 
         if (mAttached) {
             unregisterReceiver(mReceiver);
