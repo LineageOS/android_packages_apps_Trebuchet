07576f958f0fe4023559bc442a76266a65fc0bf8
Eddy Witkamp <eddywitkamp@gmail.com>
2014-06-12 11:47:10 +0000

    Revert "Improve margins for small tablets"
    Before: 
    https://www.dropbox.com/s/6hzcrci1fz4h5if/Screenshot_2014-06-12-13-09-11.png
    
    After revert: 
    https://www.dropbox.com/s/u1j336937e06mbc/Screenshot_2014-06-12-13-44-03.png
    
    
    
    This reverts commit 401aafed7995cb3b43eaae47590f4b17ae6e1ceb.
    
    Change-Id: I2ffd2f5d2998a8a1c1eac51c06d6da6dd922a722

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -241,7 +241,7 @@ public class DeviceProfile {
         searchBarVisible = SettingsProvider.getBoolean(context, SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH,
                 R.bool.preferences_interface_homescreen_search_default);
         searchBarSpaceWidthPx = Math.min(searchBarSpaceMaxWidthPx, widthPx);
-        searchBarSpaceHeightPx = 2 * edgeMarginPx + (searchBarVisible ? searchBarHeightPx  : 0);
+        searchBarSpaceHeightPx = searchBarHeightPx + (searchBarVisible ? 2 * edgeMarginPx : 0);
     }
 
     void addCallback(DeviceProfileCallbacks cb) {
@@ -651,7 +651,8 @@ public class DeviceProfile {
         // Update search bar for live settings
         searchBarVisible = SettingsProvider.getBoolean(launcher, SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH,
                 R.bool.preferences_interface_homescreen_search_default);
-        searchBarSpaceHeightPx = 2 * edgeMarginPx + (searchBarVisible ? searchBarHeightPx : 0);
+        searchBarSpaceHeightPx = searchBarHeightPx + (searchBarVisible ? 2 * edgeMarginPx : 0);
+
         FrameLayout.LayoutParams lp;
         Resources res = launcher.getResources();
         boolean hasVerticalBarLayout = isVerticalBarLayout();
