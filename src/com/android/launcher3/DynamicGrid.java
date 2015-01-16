c03184f317466870b68403aa708fcb9e283260d4
Matt Garnes <matt@cyngn.com>
2015-01-16 19:35:52 +0800

    Rename carrier workspace files and config.
    - Rename Micromax config and workspace.
    - Rename Smartfren config and workspace.
    
    Change-Id: Ic13191f11c325715eb5d6e97899e9cbc900778bd

--- src/com/android/launcher3/DynamicGrid.java
+++ src/com/android/launcher3/DynamicGrid.java
@@ -70,7 +70,7 @@ public class DynamicGrid {
         } else if (LauncherApplication.LAUNCHER_MMX_SHORTCUT_ENABLED) {
             fourByFourDefaultLayout = R.xml.micromax_default_workspace;
         } else if (LauncherApplication.LAUNCHER_SFR_SHORTCUT_ENABLED) {
-            fourByFourDefaultLayout = R.xml.smartfren_default_workspace;
+            fourByFourDefaultLayout = R.xml.srf_default_workspace;
         }
 
         DEFAULT_ICON_SIZE_PX = pxFromDp(DEFAULT_ICON_SIZE_DP, dm);
