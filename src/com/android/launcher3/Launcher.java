92600f14e5132d25ed8295419915336cfeeab03a
emancebo <emancebo@cyngn.com>
2015-02-03 12:15:11 -0800

    Trebuchet: ensure preview animation is released
    Change-Id: Id6253a6162ee47dd1a46886026f09c3adbdc7027

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1488,6 +1488,7 @@ public class Launcher extends Activity
         }
         fragmentTransaction
                 .remove(mTransitionEffectsFragment).commit();
+        mTransitionEffectsFragment = null;
 
         mDarkPanel.setVisibility(View.VISIBLE);
         ObjectAnimator anim = ObjectAnimator.ofFloat(
