df91a7904fc73a48017c5c4bba5c2c1a21cea7ec
linuxxxxx <joey@cyanogenmoditalia.it>
2015-05-05 15:19:42 +0200

    Trebuchet: I was given a name, call me so
    Change-Id: I09ece26e736c472cdc3458ced3a41cae205ec10b
    Signed-off-by: linuxxxxx <joey@cyanogenmoditalia.it>

---
+++ src/com/cyanogenmod/trebuchet/LauncherProviderChangeListener.java
@@ -0,0 +1,11 @@
+package com.cyanogenmod.trebuchet;
+
+/**
+ * This class is a listener for {@link LauncherProvider} changes. It gets notified in the
+ * sendNotify method. This listener is needed because by default the Launcher suppresses
+ * standard data change callbacks.
+ */
+public interface LauncherProviderChangeListener {
+
+    public void onLauncherProviderChange();
+}
