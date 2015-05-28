5674f45243cd48843f2a2935798839030c993fb4
Linus Lee <llee@cyngn.com>
2015-05-28 15:51:33 -0700

    Fix power save mode broken ui/animations
    Change-Id: I7dc0123300ff2e9aa64713c1091d13ecc93001f3

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -64,8 +64,6 @@ import android.view.ViewPropertyAnimator;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
 import android.view.accessibility.AccessibilityManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.widget.TextView;
@@ -2429,11 +2427,20 @@ public class Workspace extends SmoothPagedView
                         layerViews.add(cl);
                     }
                     if (mOldAlphas[i] != mNewAlphas[i] || currentAlpha != mNewAlphas[i]) {
+                        final View shortcutAndWidgets = cl.getShortcutsAndWidgets();
                         LauncherViewPropertyAnimator alphaAnim =
-                            new LauncherViewPropertyAnimator(cl.getShortcutsAndWidgets());
+                            new LauncherViewPropertyAnimator(shortcutAndWidgets);
                         alphaAnim.alpha(mNewAlphas[i])
                             .setDuration(duration)
                             .setInterpolator(mZoomInInterpolator);
+                        alphaAnim.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                // in low power mode the animation doesn't play,
+                                // so set the end value here
+                                shortcutAndWidgets.setAlpha(mNewAlphas[i]);
+                            }
+                        });
                         anim.play(alphaAnim);
                     }
                     if (mOldBackgroundAlphas[i] != 0 ||
@@ -2457,7 +2464,8 @@ public class Workspace extends SmoothPagedView
             if (pageIndicator != null) {
                 pageIndicatorAlpha = new LauncherViewPropertyAnimator(pageIndicator)
                     .alpha(finalHotseatAndPageIndicatorAlpha).withLayer();
-                pageIndicatorAlpha.addListener(new AlphaUpdateListener(pageIndicator));
+                pageIndicatorAlpha.addListener(
+                        new AlphaUpdateListener(pageIndicator, finalHotseatAndPageIndicatorAlpha));
             } else {
                 // create a dummy animation so we don't need to do null checks later
                 pageIndicatorAlpha = ValueAnimator.ofFloat(0, 0);
@@ -2465,15 +2473,19 @@ public class Workspace extends SmoothPagedView
 
             Animator hotseatAlpha = new LauncherViewPropertyAnimator(hotseat)
                 .alpha(finalHotseatAndPageIndicatorAlpha).withLayer();
-            hotseatAlpha.addListener(new AlphaUpdateListener(hotseat));
+            hotseatAlpha.addListener(
+                    new AlphaUpdateListener(hotseat, finalHotseatAndPageIndicatorAlpha));
 
             Animator searchBarAlpha = new LauncherViewPropertyAnimator(searchBar)
                 .alpha(finalSearchBarAlpha).withLayer();
-            if (mShowSearchBar) searchBarAlpha.addListener(new AlphaUpdateListener(searchBar));
+            if (mShowSearchBar) {
+                searchBarAlpha.addListener(new AlphaUpdateListener(searchBar, finalSearchBarAlpha));
+            }
 
             Animator overviewPanelAlpha = new LauncherViewPropertyAnimator(overviewPanel)
                 .alpha(finalOverviewPanelAlpha).withLayer();
-            overviewPanelAlpha.addListener(new AlphaUpdateListener(overviewPanel));
+            overviewPanelAlpha.addListener(
+                    new AlphaUpdateListener(overviewPanel, finalOverviewPanelAlpha));
 
             // For animation optimations, we may need to provide the Launcher transition
             // with a set of views on which to force build layers in certain scenarios.
@@ -2584,8 +2596,10 @@ public class Workspace extends SmoothPagedView
 
     static class AlphaUpdateListener implements AnimatorUpdateListener, AnimatorListener {
         View view;
-        public AlphaUpdateListener(View v) {
+        float endAlpha;
+        public AlphaUpdateListener(View v, float target) {
             view = v;
+            endAlpha = target;
         }
 
         @Override
@@ -2611,6 +2625,9 @@ public class Workspace extends SmoothPagedView
 
         @Override
         public void onAnimationEnd(Animator arg0) {
+            // in low power mode the animation doesn't play, so we need to set the end alpha
+            // before calling updateVisibility
+            view.setAlpha(endAlpha);
             updateVisibility(view);
         }
 
