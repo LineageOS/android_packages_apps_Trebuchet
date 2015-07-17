3f4665143966913a813695482f6fab81d490fc54
d34d <clark@cyngn.com>
2015-07-17 10:06:20 -0700

    Show apps as unavialable when on unmounted storage
    This patch allows shortcuts to apps that are currently unavailable
    due to being on unmounted external storage to remain on the workspace
    and in folders.  The icons will be disabled and grayed out until
    the external storage is mounted.
    
    Change-Id: I971483806b27e3a75ef0d5bb89d8dfb86f97511e

--- src/com/android/launcher3/LauncherModel.java
+++ src/com/android/launcher3/LauncherModel.java
@@ -211,6 +211,9 @@ public class LauncherModel extends BroadcastReceiver
         public void updatePackageBadge(String packageName);
         public void bindComponentsRemoved(ArrayList<String> packageNames,
                         ArrayList<AppInfo> appInfos, UserHandleCompat user);
+        public void bindComponentsUnavailable(ArrayList<String> packageNames,
+                ArrayList<AppInfo> appInfos);
+        public void bindComponentsAvailable(ArrayList<ItemInfo> itemInfos);
         public void bindPackagesUpdated(ArrayList<Object> widgetsAndShortcuts);
         public void bindSearchablesChanged();
         public boolean isAllAppsButtonRank(int rank);
@@ -3367,6 +3370,7 @@ public class LauncherModel extends BroadcastReceiver
 
             final String[] packages = mPackages;
             final int N = packages.length;
+            final ArrayList<String> unavailable = new ArrayList<String>();
             switch (mOp) {
                 case OP_ADD:
                     for (int i=0; i<N; i++) {
@@ -3390,6 +3394,9 @@ public class LauncherModel extends BroadcastReceiver
                         mBgAllAppsList.removePackage(packages[i], mUser);
                         WidgetPreviewLoader.removePackageFromDb(
                                 mApp.getWidgetPreviewCacheDb(), packages[i]);
+                        if (mOp == OP_UNAVAILABLE) {
+                            unavailable.add(packages[i]);
+                        }
                     }
                     break;
             }
@@ -3418,12 +3425,21 @@ public class LauncherModel extends BroadcastReceiver
             }
 
             if (added != null) {
+                final ArrayList<ItemInfo> addedInfos = new ArrayList<ItemInfo>(added);
                 // Ensure that we add all the workspace applications to the db
                 if (LauncherAppState.isDisableAllApps()) {
-                    final ArrayList<ItemInfo> addedInfos = new ArrayList<ItemInfo>(added);
                     addAndBindAddedWorkspaceApps(context, addedInfos);
                 } else {
                     addAppsToAllApps(context, added);
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            Callbacks cb = mCallbacks != null ? mCallbacks.get() : null;
+                            if (callbacks == cb && cb != null) {
+                                Log.d(TAG, "bindComponentsAvailable: " + addedInfos.size());
+                                callbacks.bindComponentsAvailable(addedInfos);
+                            }
+                        }
+                    });
                 }
             }
 
@@ -3468,27 +3484,40 @@ public class LauncherModel extends BroadcastReceiver
                     }
                 }
             }
-            // Remove all the components associated with this package
-            for (String pn : removedPackageNames) {
-                deletePackageFromDatabase(context, pn, mUser);
-            }
-            // Remove all the specific components
-            for (AppInfo a : removedApps) {
-                ArrayList<ItemInfo> infos = getItemInfoForComponentName(a.componentName, mUser);
-                deleteItemsFromDatabase(context, infos);
-            }
-            if (!removedPackageNames.isEmpty() || !removedApps.isEmpty()) {
-                // Remove any queued items from the install queue
-                String spKey = LauncherAppState.getSharedPreferencesKey();
-                SharedPreferences sp =
-                        context.getSharedPreferences(spKey, Context.MODE_PRIVATE);
-                InstallShortcutReceiver.removeFromInstallQueue(sp, removedPackageNames);
-                // Call the components-removed callback
+            if (unavailable.isEmpty()) {
+                // Remove all the components associated with this package
+                for (String pn : removedPackageNames) {
+                    deletePackageFromDatabase(context, pn, mUser);
+                }
+                // Remove all the specific components
+                for (AppInfo a : removedApps) {
+                    ArrayList<ItemInfo> infos = getItemInfoForComponentName(a.componentName, mUser);
+                    deleteItemsFromDatabase(context, infos);
+                }
+                if (!removedPackageNames.isEmpty() || !removedApps.isEmpty()) {
+                    // Remove any queued items from the install queue
+                    String spKey = LauncherAppState.getSharedPreferencesKey();
+                    SharedPreferences sp =
+                            context.getSharedPreferences(spKey, Context.MODE_PRIVATE);
+                    InstallShortcutReceiver.removeFromInstallQueue(sp, removedPackageNames);
+                    // Call the components-removed callback
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            Callbacks cb = mCallbacks != null ? mCallbacks.get() : null;
+                            if (callbacks == cb && cb != null) {
+                                callbacks.bindComponentsRemoved(removedPackageNames, removedApps,
+                                        mUser);
+                            }
+                        }
+                    });
+                }
+            } else {
+                // Call the packages-unavailable callback
                 mHandler.post(new Runnable() {
                     public void run() {
                         Callbacks cb = mCallbacks != null ? mCallbacks.get() : null;
                         if (callbacks == cb && cb != null) {
-                            callbacks.bindComponentsRemoved(removedPackageNames, removedApps, mUser);
+                            callbacks.bindComponentsUnavailable(unavailable, removedApps);
                         }
                     }
                 });
