ae9c75937aa2786ed9a490e9464b1de63f2d2e07
Roman Birg <roman@cyngn.com>
2015-11-03 10:18:22 -0800

    Trebuchet: fix crash when no search component exists
    Ref: CYNGNOS-1290
    
    Change-Id: I340e88239812f69151de30ccf18d66ed9ab14d85
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -4619,6 +4619,9 @@ public class Launcher extends Activity
             return null;
         }
         ComponentName searchComponent = assistIntent.getComponent();
+        if (searchComponent == null) {
+            return null;
+        }
 
         // Find the first widget from the same package as the global assist activity
         List<AppWidgetProviderInfo> widgets = AppWidgetManager.getInstance(this)
