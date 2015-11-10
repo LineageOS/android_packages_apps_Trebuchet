53ed6dc8201204df47de8dfc542b406118168feb
Tyson Miller <tmiller@cyngn.com>
2015-11-10 08:59:15 -0800

    Port Remote Folder from 12.1 to 13.
    Change-Id: If8cf9d5f054e8948ead702883b79f28db26c4d8b

--- src/com/android/launcher3/allapps/AllAppsContainerView.java
+++ src/com/android/launcher3/allapps/AllAppsContainerView.java
@@ -204,12 +204,33 @@ public class AllAppsContainerView extends BaseContainerView implements DragSourc
         Selection.setSelection(mSearchQueryBuilder, 0);
     }
 
+    public int getNumPredictedAppsPerRow() {
+        return mNumPredictedAppsPerRow;
+    }
+
     /**
-     * Sets the current set of predicted apps.
+     * Sets the current set of predicted apps by component.
+     * Only usable when custom predicted apps are disabled.
      */
-    public void setPredictedApps(List<ComponentKey> apps) {
+    public void setPredictedAppComponents(List<ComponentKey> apps) {
+        mApps.setPredictedAppComponents(apps);
+        updateScrubber();
+    }
+
+    /**
+     * Sets the current set of predicted apps by info.
+     * Only usable when custom predicated apps are enabled.
+     */
+    public void setPredictedApps(List<AppInfo> apps) {
         mApps.setPredictedApps(apps);
         updateScrubber();
+    }
+
+    /**
+     * Set whether the predicted apps row will have a customized selection of apps.
+     */
+    public void setCustomPredictedAppsEnabled(boolean enabled) {
+        mApps.mCustomPredictedAppsEnabled = enabled;
     }
 
     /**
