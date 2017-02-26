395d1546b5887031a705d0266670bf87696a2b4f
Thecrazyskull <anaskarbila@gmail.com>
2017-02-26 17:03:46 -0500

    Prevent crash
    We can only set SystemUI visibility from the UI thread
    
    Change-Id: I1ecf1fcddef037da5ecd2e0f53989da82bdf7ef0

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -520,7 +520,10 @@ public class Launcher extends BaseActivity
         }
 
         if (newSystemUiFlags != oldSystemUiFlags) {
-            getWindow().getDecorView().setSystemUiVisibility(newSystemUiFlags);
+            final int systemUiFlags = newSystemUiFlags;
+            runOnUiThread(() -> {
+                getWindow().getDecorView().setSystemUiVisibility(systemUiFlags);
+            });
         }
     }
 
