2f9fe5b4d854afb01ad68d2d938a0048db6dddf9
Arne Coucheron <arco68@gmail.com>
2017-06-28 02:20:46 +0200

    Disable QSB on first screen by default
    Change-Id: I24f1682cbad10610f04f7b8ada238b43addd23da

--- src/com/android/launcher3/config/BaseFlags.java
+++ src/com/android/launcher3/config/BaseFlags.java
@@ -67,7 +67,7 @@ abstract class BaseFlags {
     public static final boolean LAUNCHER3_PROMISE_APPS_IN_ALL_APPS = true;
 
     // Enable moving the QSB on the 0th screen of the workspace
-    public static final boolean QSB_ON_FIRST_SCREEN = true;
+    public static final boolean QSB_ON_FIRST_SCREEN = false;
 
     public static final TogglableFlag EXAMPLE_FLAG = new TogglableFlag("EXAMPLE_FLAG", true,
             "An example flag that doesn't do anything. Useful for testing");
