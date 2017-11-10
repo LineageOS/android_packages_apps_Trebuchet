2858a5f20dd2f2471cb862acebcba06a886934e2
Yajat <yajatapps3@gmail.com>
2017-11-10 21:08:21 +0100

    Trebuchet: predictive apps
    jrizzoli: expose for runtime toggle
    
    Signed-off-by: Joey <joey@lineageos.org>

--- src/com/android/launcher3/config/BaseFlags.java
+++ src/com/android/launcher3/config/BaseFlags.java
@@ -61,7 +61,7 @@ abstract class BaseFlags {
     // When enabled, adaptive icons would have shadows baked when being stored to icon cache.
     public static final boolean ADAPTIVE_ICON_SHADOW = true;
     // When enabled, app discovery will be enabled if service is implemented
-    public static final boolean DISCOVERY_ENABLED = false;
+    public static final boolean DISCOVERY_ENABLED = true;
     // When enabled, the qsb will be moved to the hotseat.
     public static final boolean QSB_IN_HOTSEAT = true;
 
