3cdbb7e3c7ecae0fda2f906c720ea07b6eb8326f
Adrian DC <radian.dc@gmail.com>
2017-12-07 22:42:34 +0100

    Trebuchet: Hide Notification Dots on low RAM devices
     * As the feature is not available for low RAM devices,
       hide it from the Settings to avoid confusion
    
     * Acquire ActivityManager service to read isLowRamDevice
    
    Change-Id: I34a6ee1b4838f31194c89327e301d4fd39670c02
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -22,6 +22,7 @@ import static com.android.launcher3.states.RotationHelper.getAllowRotationDefaul
 import static com.android.launcher3.util.SecureSettingsObserver.newNotificationSettingsObserver;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.ComponentName;
@@ -199,6 +200,7 @@ public class SettingsActivity extends Activity
             switch (preference.getKey()) {
                 case NOTIFICATION_DOTS_PREFERENCE_KEY:
                     if (!Utilities.ATLEAST_OREO ||
+                            getContext().getSystemService(ActivityManager.class).isLowRamDevice() ||
                             !getResources().getBoolean(R.bool.notification_dots_enabled)) {
                         return false;
                     }
