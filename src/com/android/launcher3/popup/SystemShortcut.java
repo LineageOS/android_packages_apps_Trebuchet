521a6b1fdbc5a72b5c44c218d779ef05f226abf0
Joey <joey@lineageos.org>
2018-02-04 11:36:32 +0100

    Trebuchet: allow disabling workspace edit
    Co-authored-by: Han Wang <416810799@qq.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Change-Id: I503e19cbc512eac0e4a8c8bccc16a6ccc0e805da

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -125,6 +125,7 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
 
         @Override
         public void onClick(View view) {
+            if (!Utilities.isWorkspaceEditAllowed((Context) mTarget)) return;
             AbstractFloatingView.closeAllOpenViews(mTarget);
             WidgetsBottomSheet widgetsBottomSheet =
                     (WidgetsBottomSheet) mTarget.getLayoutInflater().inflate(
