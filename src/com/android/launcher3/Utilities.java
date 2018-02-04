21f773ba0c60fba47e078c7cf1dc05e8ef4f4e47
Joey <joey@lineageos.org>
2018-02-04 11:36:32 +0100

    Trebuchet: allow disabling workspace edit
    Change-Id: I503e19cbc512eac0e4a8c8bccc16a6ccc0e805da
    Signed-off-by: Joey <joey@lineageos.org>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -689,4 +689,9 @@ public final class Utilities {
             return mSize;
         }
     }
+
+    public static boolean isWorkspaceEditAllowed(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_EDIT, true);
+    }
 }
