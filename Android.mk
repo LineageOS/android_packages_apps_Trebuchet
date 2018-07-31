dd20f71795f90eaf9e0d6ad47ba8c658db4b7a83
Edward Cross <edward.k.cross@gmail.com>
2018-07-31 15:17:57 +0200

    Launcher3: Google Feed integration
    If Google Search is installed, the Google Feed can be found at
    the -1 screen (which is the very left).
    This feature is enabled by default.
    
    Change-Id: I495196818699fd378cd60e6dd61b07a0ab951762

--- Android.mk
+++ Android.mk
@@ -17,6 +17,18 @@
 LOCAL_PATH := $(call my-dir)
 
 #
+# Prebuilt Google Feed library
+#
+include $(CLEAR_VARS)
+LOCAL_MODULE := libGoogleFeed
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_SRC_FILES := libs/libGoogleFeed.jar
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_SDK_VERSION := 27
+include $(BUILD_PREBUILT)
+
+#
 # Build rule for Launcher3 dependencies lib.
 #
 include $(CLEAR_VARS)
@@ -32,7 +44,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     LauncherPluginLib \
-    launcher_log_protos_lite
+    launcher_log_protos_lite \
+    libGoogleFeed
 
 LOCAL_SRC_FILES := \
     $(call all-proto-files-under, protos) \
