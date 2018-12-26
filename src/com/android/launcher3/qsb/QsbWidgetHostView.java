fe9c56fe329cc7c8f01aef929cb66fe0904dea1a
Edward Cross <edward.k.cross@gmail.com>
2018-12-26 22:18:30 +0100

    QSB: request round search bar
    Change-Id: Icf81d36e467fdbe5cc4e4a081c8e3ec2ebaa4263

--- src/com/android/launcher3/qsb/QsbWidgetHostView.java
+++ src/com/android/launcher3/qsb/QsbWidgetHostView.java
@@ -17,6 +17,7 @@
 package com.android.launcher3.qsb;
 
 import android.appwidget.AppWidgetHostView;
+import android.appwidget.AppWidgetProviderInfo;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,6 +47,11 @@ public class QsbWidgetHostView extends AppWidgetHostView {
         super.updateAppWidget(remoteViews);
     }
 
+    @Override
+    public void setAppWidget(int appWidgetId, AppWidgetProviderInfo info) {
+        QsbContainerView.updateDefaultLayout(getContext(), info);
+        super.setAppWidget(appWidgetId, info);
+    }
 
     public boolean isReinflateRequired(int orientation) {
         // Re-inflate is required if the orientation has changed since last inflation.
