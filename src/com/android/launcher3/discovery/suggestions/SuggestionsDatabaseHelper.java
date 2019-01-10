1141fe81001e35e345f435a9ec9a326ced65af7d
razorloves <razorloves@gmail.com>
2019-01-09 21:41:15 -0600

    Discovery: Add missing SQLiteFullException import
    Fix for d17b08e4a
    
    Change-Id: I24fb9423ec6d0070cbf682c87e9bab9c0f368ffe

--- src/com/android/launcher3/discovery/suggestions/SuggestionsDatabaseHelper.java
+++ src/com/android/launcher3/discovery/suggestions/SuggestionsDatabaseHelper.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteFullException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
