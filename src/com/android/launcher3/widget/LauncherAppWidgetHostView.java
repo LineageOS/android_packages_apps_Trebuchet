7a40ad1dc9291ea50feb87ce737a640536c4af5b
Michael W <baddaemon87@gmail.com>
2021-11-27 18:42:35 +0100

    Trebuchet: Don't show move layer when editing isn't allowed
    * When long clicking a widget, it behaves like you could move it just
      to immediately return to default state when moving the finger
    * Just don't react to long pressing a widget when editing is disallowed
    
    Change-Id: If5d8558c6311b569c7b21e8b4b6598f0dbdd3319

--- src/com/android/launcher3/widget/LauncherAppWidgetHostView.java
+++ src/com/android/launcher3/widget/LauncherAppWidgetHostView.java
@@ -112,6 +112,7 @@ public class LauncherAppWidgetHostView extends BaseLauncherAppWidgetHostView
 
     @Override
     public boolean onLongClick(View view) {
+        if (!Utilities.isWorkspaceEditAllowed(mLauncher.getApplicationContext())) return true;
         if (mIsScrollable) {
             DragLayer dragLayer = mLauncher.getDragLayer();
             dragLayer.requestDisallowInterceptTouchEvent(false);
