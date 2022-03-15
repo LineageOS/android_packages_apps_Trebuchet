c3e9f23c60b54abb639bed1dc4789c61b00583c9
jhonboy121 <alfredmathew05@gmail.com>
2022-03-15 18:55:35 +0530

    Launcher3: Show clear all button in recents overview
    Change-Id: I90eacea0bc18436c825dac74dcd2d6e48d1d85d6

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -217,6 +217,11 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             overviewPanel.initiateSplitSelect(mThumbnailView.getTaskView());
         }
 
+        private void clearAllTasks() {
+            final RecentsView recentsView = mThumbnailView.getTaskView().getRecentsView();
+            recentsView.dismissAllTasks();
+        }
+
         /**
          * Called when the overlay is no longer used.
          */
@@ -322,12 +327,19 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             }
 
             @SuppressLint("NewApi")
+            @Override
             public void onScreenshot() {
                 endLiveTileMode(() -> saveScreenshot(mTask));
             }
 
+            @Override
             public void onSplit() {
                 endLiveTileMode(TaskOverlay.this::enterSplitSelect);
+            }
+
+            @Override
+            public void onClearAllTasksRequested() {
+                endLiveTileMode(TaskOverlay.this::clearAllTasks);
             }
         }
     }
@@ -342,5 +354,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
 
         /** User wants to start split screen with current app. */
         void onSplit();
+
+        void onClearAllTasksRequested();
     }
 }
