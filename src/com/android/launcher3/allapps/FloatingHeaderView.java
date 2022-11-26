fba6d33c5916189137ce67313a2672b1649c8dfb
Tommy Webb <tommy@calyxinstitute.org>
2022-11-25 21:14:06 -0500

    Revert "Fix header protection height calculation"
    This reverts commit b4873ddf4ec9594423ad379f1ed9b0a43af5457f.
    
    Change-Id: Ida4651d364ead5f182fc60a9aec6788317b2422c

--- src/com/android/launcher3/allapps/FloatingHeaderView.java
+++ src/com/android/launcher3/allapps/FloatingHeaderView.java
@@ -321,7 +321,7 @@ public class FloatingHeaderView extends LinearLayout implements
 
         int clipTop = getPaddingTop() - mHeaderTopAdjustment;
         if (mTabsHidden) {
-            clipTop += getPaddingBottom() * 2 - mHeaderBottomAdjustment;
+            clipTop += getPaddingBottom() - mHeaderBottomAdjustment;
         }
         mRVClip.top = mTabsHidden ? clipTop : 0;
         mHeaderClip.top = clipTop;
@@ -444,6 +444,6 @@ public class FloatingHeaderView extends LinearLayout implements
         if (mTabsHidden || !mHeaderCollapsed) {
             return 0;
         }
-        return Math.max(getHeight() - getPaddingTop() + mTranslationY + getPaddingBottom(), 0);
+        return Math.max(getHeight() - getPaddingTop() + mTranslationY, 0);
     }
 }
