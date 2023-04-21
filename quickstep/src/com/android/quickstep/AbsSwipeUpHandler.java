599689c7a76fc6b88274cc1219feaa8185c4bf08
nift4 <nift4@protonmail.com>
2023-04-21 18:26:23 +0200

    Trebuchet: Let's keep 2-button nav alive for a little longer
    * Refactor to avoid breaking gesture nav
    
    This partially reverts commit 9cddf4133befc721f2eb6a374b40fbb1b74f64ef.
    This partially reverts commit 0130252c00ef268dfb80d9bff9c3da90bfffd7ff.
    
    Change-Id: I1823de2a911f8a17c3716bc0b1b8e6c9c55d37cd

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -305,6 +305,7 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
 
     private boolean mWasLauncherAlreadyVisible;
 
+    private boolean mPassedOverviewThreshold;
     private boolean mGestureStarted;
     private boolean mLogDirectionUpOrLeft = true;
     private boolean mIsLikelyToStartNewTask;
@@ -883,6 +884,14 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
     @UiThread
     @Override
     public void onCurrentShiftUpdated() {
+        final boolean passed = hasReachedHomeOverviewThreshold();
+        if (passed != mPassedOverviewThreshold) {
+            mPassedOverviewThreshold = passed;
+            if (mDeviceState.isTwoButtonNavMode() && !mGestureState.isHandlingAtomicEvent()) {
+                performHapticFeedback();
+            }
+        }
+
         float threshold = LauncherPrefs.get(mContext).get(ALL_APPS_OVERVIEW_THRESHOLD) / 100f;
         setIsInAllAppsRegion(mCurrentShift.value >= threshold);
         updateSysUiFlags(mCurrentShift.value);
@@ -981,6 +990,8 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
         mStateCallback.runOnceAtState(STATE_APP_CONTROLLER_RECEIVED | STATE_GESTURE_STARTED,
                 this::startInterceptingTouchesForGesture);
         mStateCallback.setStateOnUiThread(STATE_APP_CONTROLLER_RECEIVED);
+
+        mPassedOverviewThreshold = false;
     }
 
     @Override
@@ -1260,11 +1271,19 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
             return willGoToNewTask || isCenteredOnNewTask ? NEW_TASK : LAST_TASK;
         }
 
+        if (!mDeviceState.isFullyGesturalNavMode()) {
+            return (!hasReachedHomeOverviewThreshold() && willGoToNewTask) ? NEW_TASK : RECENTS;
+        }
         return willGoToNewTask ? NEW_TASK : HOME;
     }
 
     private GestureEndTarget calculateEndTargetForNonFling(PointF velocity) {
         final boolean isScrollingToNewTask = isScrollingToNewTask();
+        if (!mDeviceState.isFullyGesturalNavMode()) {
+            return hasReachedHomeOverviewThreshold() && mGestureStarted
+                    ? RECENTS
+                    : (isScrollingToNewTask ? NEW_TASK : LAST_TASK);
+        }
 
         // Fully gestural mode.
         final boolean isFlingX = Math.abs(velocity.x) > mContext.getResources()
@@ -1306,6 +1325,16 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
      */
     public void setCanSlowSwipeGoHome(boolean canSlowSwipeGoHome) {
         mCanSlowSwipeGoHome = canSlowSwipeGoHome;
+    }
+
+    /**
+     * Returns true if swipe has reached the overview threshold.
+     */
+    private boolean hasReachedHomeOverviewThreshold() {
+        if (mIsTransientTaskbar) {
+            return mCanSlowSwipeGoHome;
+        }
+        return mCurrentShift.value > MIN_PROGRESS_FOR_OVERVIEW;
     }
 
     @UiThread
