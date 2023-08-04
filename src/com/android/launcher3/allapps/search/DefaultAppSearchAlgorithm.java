aee7638c78895be36e4b4f3eb43db482b676b270
Sebastian Hasch <haschsebastian@gmail.com>
2023-08-04 15:15:01 +0200

    Remove 5 element search result limit in app drawer
    Removes the 5 element search result limit in the app drawer so
    all matches get displayed when searching an app.
    
    Change-Id: I41cf69bb92346abeef78fcf85da4769d6e2c146b

--- src/com/android/launcher3/allapps/search/DefaultAppSearchAlgorithm.java
+++ src/com/android/launcher3/allapps/search/DefaultAppSearchAlgorithm.java
@@ -42,8 +42,6 @@ import java.util.List;
  */
 public class DefaultAppSearchAlgorithm implements SearchAlgorithm<AdapterItem> {
 
-    private static final int MAX_RESULTS_COUNT = 5;
-
     private final LauncherAppState mAppState;
     private final Handler mResultHandler;
     private final boolean mAddNoResultsMessage;
@@ -101,13 +99,11 @@ public class DefaultAppSearchAlgorithm implements SearchAlgorithm<AdapterItem> {
         StringMatcherUtility.StringMatcher matcher =
                 StringMatcherUtility.StringMatcher.getInstance();
 
-        int resultCount = 0;
         int total = apps.size();
-        for (int i = 0; i < total && resultCount < MAX_RESULTS_COUNT; i++) {
+        for (int i = 0; i < total; i++) {
             AppInfo info = apps.get(i);
             if (StringMatcherUtility.matches(queryTextLower, info.title.toString(), matcher)) {
                 result.add(AdapterItem.asApp(info));
-                resultCount++;
             }
         }
         return result;
