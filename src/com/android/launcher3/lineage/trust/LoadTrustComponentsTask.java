fd829be2469961311018477785c55919d7f22fdd
Bruno Martins <bgcngm@gmail.com>
2024-11-22 11:29:41 +0000

    trust: Respect filtered components when showing apps list
    Launcher3 nativelly supports filtering certain components via the
    overlay config "filtered_components", making it not visible in some
    places such as the apps drawer. Respect this config as well in the
    custom "Hidden & Protected apps" feature.
    
    Change-Id: Ie4e9b35c616b0f242c6c971b6bbb5280b1f4480f

--- src/com/android/launcher3/lineage/trust/LoadTrustComponentsTask.java
+++ src/com/android/launcher3/lineage/trust/LoadTrustComponentsTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@ import android.os.Build;
 
 import androidx.annotation.NonNull;
 
+import com.android.launcher3.AppFilter;
 import com.android.launcher3.lineage.trust.db.TrustComponent;
 import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 
@@ -62,6 +63,11 @@ public class LoadTrustComponentsTask extends AsyncTask<Void, Integer, List<Trust
         int numPackages = apps.size();
         for (int i = 0; i < numPackages; i++) {
             ResolveInfo app = apps.get(i);
+
+            if (AppFilter.getFilteredComponents().contains(app.activityInfo.getComponentName())) {
+                continue;
+            }
+
             try {
                 String pkgName = app.activityInfo.packageName;
                 String label = mPackageManager.getApplicationLabel(
