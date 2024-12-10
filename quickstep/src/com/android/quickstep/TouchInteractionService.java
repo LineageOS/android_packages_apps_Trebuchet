b1251f5fe5e86b8aa62b184b24d2f0cf2903a69c
Tony Wickham <twickham@google.com>
2024-12-10 22:29:30 +0000

    Add onUnbind from OverviewProxyService, and destroy taskbar
    When unbound, destroy taskbar immediately instead of waiting for
    TIS#onDestroy. And then send the reply back to OverviewProxyService
    so it can bind to a new TIS.
    
    Fixes: 375315062
    Test: Switch users 10 times, no crash (also check logs)
    Flag: EXEMPT bug fix
    
    Change-Id: I737c47c7df87e11608672a4e16eb3e9fd645e487

--- quickstep/src/com/android/quickstep/TouchInteractionService.java
+++ quickstep/src/com/android/quickstep/TouchInteractionService.java
@@ -48,7 +48,9 @@ import android.graphics.Region;
 import android.hardware.input.InputManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.IRemoteCallback;
 import android.os.Looper;
+import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.Trace;
 import android.util.ArraySet;
@@ -398,6 +400,20 @@ public class TouchInteractionService extends Service {
         public void onNavigationBarLumaSamplingEnabled(int displayId, boolean enable) {
             executeForTaskbarManager(taskbarManager ->
                     taskbarManager.onNavigationBarLumaSamplingEnabled(displayId, enable));
+        }
+
+        @Override
+        public void onUnbind(IRemoteCallback reply) {
+            // Run everything in the same main thread block to ensure the cleanup happens before
+            // sending the reply.
+            MAIN_EXECUTOR.execute(() -> {
+                executeForTaskbarManager(TaskbarManager::destroy);
+                try {
+                    reply.sendResult(null);
+                } catch (RemoteException e) {
+                    Log.w(TAG, "onUnbind: Failed to reply to OverviewProxyService", e);
+                }
+            });
         }
 
         private void executeForTouchInteractionService(
