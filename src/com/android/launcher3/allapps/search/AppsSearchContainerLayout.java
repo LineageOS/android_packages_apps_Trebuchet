a73e54c84788bb24fb8ab88dbcf3e22b93d9cc40
Oliver Scott <olivercscott@gmail.com>
2024-12-30 15:01:22 -0500

    fixup! Implement private space access when hidden
    Change-Id: I2b44e11a6b4ac96abcccdeaa0c3974c9cca6ae5e

--- src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
+++ src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -168,7 +168,7 @@ public class AppsSearchContainerLayout extends ExtendedEditText
 
     @Override
     public void onSearchResult(String query, ArrayList<AdapterItem> items) {
-        if (query.equalsIgnoreCase(mContext.getString(R.string.private_space_label))) {
+        if (query.equalsIgnoreCase(getContext().getString(R.string.private_space_label))) {
             privateSpaceQuery();
             return;
         }
@@ -204,7 +204,7 @@ public class AppsSearchContainerLayout extends ExtendedEditText
             privateProfileManager.setQuietMode(false);
         } else if (!mAppsView.hasPrivateProfile()) {
             final Intent privateSpaceSettingsIntent =
-                    ApiWrapper.INSTANCE.get(mContext).getPrivateSpaceSettingsIntent();
+                    ApiWrapper.INSTANCE.get(getContext()).getPrivateSpaceSettingsIntent();
             if (privateSpaceSettingsIntent != null) {
                 mLauncher.startActivitySafely(mAppsView, privateSpaceSettingsIntent, null);
             }
